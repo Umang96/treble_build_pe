1cc86ff0584d2442e117b984d49effe0c7b5ba09
Alberto Ponces <ponces26@gmail.com>
2022-02-08 22:59:17 +0000

    Fix OTA file generation

--- build.sh
+++ build.sh
@@ -115,23 +115,25 @@ generatePackages() {
 generateOtaJson() {
     prefix="PixelExperience_"
     suffix="-12.0-$BUILD_DATE-UNOFFICIAL.img.xz"
-    json="{\"version\": \"$VERSION\",\"date\": \"$(date +%s)\",\"variants\": ["
-    find $BD -name "*.img.xz" | while read file; do
-        packageVariant=$(echo $(basename $file) | sed -e s/^$prefix// -e s/$suffix$//)
-        case $packageVariant in
-            "arm32_binder64-ab") name="treble_a64_bvS";;
-            "arm32_binder64-ab-vndklite") name="treble_a64_bvS-vndklite";;
-            "arm32_binder64-ab-slim") name="treble_a64_bvS-slim";;
-            "arm64-ab") name="treble_arm64_bvS";;
-            "arm64-ab-vndklite") name="treble_arm64_bvS-vndklite";;
-            "arm64-ab-slim") name="treble_arm64_bvS-slim";;
-        esac
-        size=$(wc -c $file | awk '{print $1}')
-        url="https://github.com/ponces/treble_build_pe/releases/download/$VERSION/$(basename $file)"
-        json="${json} {\"name\": \"$name\",\"size\": \"$size\",\"url\": \"$url\"},"
-    done
-    json="${json%?}]}"
-    echo "$json" | jq . > $BL/ota.json
+    json="{\"version\": \"$VERSION\",\"date\": \"$(date +%s -d '-8hours')\",\"variants\": ["
+    find $BD -name "*.img.xz" | {
+        while read file; do
+            packageVariant=$(echo $(basename $file) | sed -e s/^$prefix// -e s/$suffix$//)
+            case $packageVariant in
+                "arm32_binder64-ab") name="treble_a64_bvS";;
+                "arm32_binder64-ab-vndklite") name="treble_a64_bvS-vndklite";;
+                "arm32_binder64-ab-slim") name="treble_a64_bvS-slim";;
+                "arm64-ab") name="treble_arm64_bvS";;
+                "arm64-ab-vndklite") name="treble_arm64_bvS-vndklite";;
+                "arm64-ab-slim") name="treble_arm64_bvS-slim";;
+            esac
+            size=$(wc -c $file | awk '{print $1}')
+            url="https://github.com/ponces/treble_build_pe/releases/download/$VERSION/$(basename $file)"
+            json="${json} {\"name\": \"$name\",\"size\": \"$size\",\"url\": \"$url\"},"
+        done
+        json="${json%?}]}"
+        echo "$json" | jq . > $BL/ota.json
+    }
 }
 
 buildTrebleApp
