14a782be09e62062c32d95a0a97e8a38a0326597
Alberto Ponces <ponces26@gmail.com>
2022-02-21 00:41:35 +0000

    Add support for Quick Tap gesture and Now Playing overlay

--- pe.mk
+++ pe.mk
@@ -6,3 +6,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.system.ota.json_url=https://raw.githubusercontent.com/ponces/treble_build_pe/twelve/ota.json
+
+PRODUCT_PACKAGES += NowPlayingOverlay
+
+TARGET_SUPPORTS_QUICK_TAP := true
